29a7d9e07639fd2c8f84634a33dfe2b2af866010
CoolUser12 <hajiyousaf034@gmail.com>
2024-06-08 15:41:20 +0500

    java code for login password

---
+++ progam.js
@@ -0,0 +1,8 @@
+// login access code
+let username = prompt("enter your username")
+let password = prompt("enter your password")
+if(username === "ahmad raza" || password==="pakistan"){
+    alert("your login is succesful");
+}else{
+    alert("our acess is denied you");
+}
